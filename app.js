d75e84f0eb68bad37be0c4fac64adf85cf141f00
guopanGit <199199516745@163.com>
2021-11-01 10:14:07 +0800

    更新

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 App({
-  onLaunch: function () {
+  onLaunch (options) {
 
+    // 检查更新
+    const updateManager = tt.getUpdateManager();
+    updateManager.onCheckForUpdate((res) =>{
+      if(res.hasUpdate){
+        updateManager.onUpdateReady((res) => {
+         tt.showModal({
+          title: "更新提示",
+          content: "新版本已经准备好，是否重启小程序？",
+          success: (res) => {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate();
+            }
+          }
+         })
+        })
+      }
+    })
+    
   }
 })
