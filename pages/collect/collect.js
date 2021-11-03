58bd4b4bc7400a77ea1c5e1ccbc8647d548642a6
guopanGit <199199516745@163.com>
2021-11-03 19:47:53 +0800

    收藏缺省图

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,9 +1,21 @@
 // /Users/mac/Documents/work/staff/参谋/pages/collect/collect.js
 Page({
   data: {
-
+    navData:{},
+    list:0
   },
-  onLoad: function (options) {
-
+  onLoad (options) {
+    let pages = getCurrentPages().length; 
+    let navData = {
+      title:'收藏',
+      pages,
+      bgcolor: "#fff",
+      color:'#333'
+    }
+    let height = getApp().globalData.height
+    this.setData({
+      navData,
+      height
+    })
   }
 })
