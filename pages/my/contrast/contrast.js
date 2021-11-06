658a962a1155c06dcaba08074a8d484147698125
guopanGit <199199516745@163.com>
2021-11-06 19:39:08 +0800

    头部导航对齐修改

--- pages/my/contrast/contrast.js
+++ pages/my/contrast/contrast.js
@@ -1,31 +1,31 @@
 // 同行业对比.js
 import {
-  showToast
+    showToast
 } from '../../../utils/utils';
 
 Page({
-  data: {
-    navData:{}
-  },
-  onLoad(options) {
-    let pages = getCurrentPages().length; 
-    let navData = {
-      title:'同业对比记录',
-      pages,
-      bgcolor : '#fff',
-      color : '#333'
-    }
-    let height = getApp().globalData.height
-    this.setData({
-      navData,
-      height
-    })
-  },
+    data: {
+        navData: {}
+    },
+    onLoad(options) {
+        let pages = getCurrentPages().length;
+        let navData = {
+            title: '行业对比记录',
+            pages,
+            bgcolor: '#fff',
+            color: '#333'
+        }
+        let height = getApp().globalData.height
+        this.setData({
+            navData,
+            height
+        })
+    },
 
-  toDetails(e){
-    console.log(e)
-    let val = e.currentTarget.dataset.val;
-    console.log(val)
-    showToast('对比结果')
-   }
+    toDetails(e) {
+        console.log(e)
+        let val = e.currentTarget.dataset.val;
+        console.log(val)
+        showToast('对比结果')
+    }
 })
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,9 +1,21 @@
 // 创业测试.js
 Page({
-  data: {
-
-  },
-  onLoad: function (options) {
-
-  }
+    data: {
+        navData: {},
+        height: 0
+    },
+    onLoad(options) {
+        let pages = getCurrentPages().length;
+        let navData = {
+            title: '项目喜好测试',
+            pages,
+            bgcolor: "#3d74f3",
+            color: '#fff'
+        }
+        let height = getApp().globalData.height
+        this.setData({
+            navData,
+            height
+        })
+    }
 })
