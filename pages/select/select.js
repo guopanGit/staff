893ebd767072409a9ab5ef67cf70edba65e82243
guopanGit <199199516745@163.com>
2021-11-09 15:23:19 +0800

    测试页面

--- pages/select/select.js
+++ pages/select/select.js
@@ -1,65 +1,90 @@
 // 精选.js
 
 import {
- getType
+    getType
 } from '../../utils/utils'
 
 Page({
-  data: {
-    navData:{
-      title:'精选'
+    data: {
+        navData: {
+            title: '精选'
+        },
+        type: true,
+        height: 0,
+        index: 0,
+        title: 1,
+        oldTop: 0,
+        isFixed: false,
+        imgUrls: [
+            '/image/grop.jpg',
+            '/image/grop.jpg',
+            '/image/grop.jpg',
+            '/image/grop.jpg'
+        ]
+
     },
-    type:true,
-    index:0,
-    title:1,
-    imgUrls:[
-      '/image/grop.jpg',
-      '/image/grop.jpg',
-      '/image/grop.jpg',
-      '/image/grop.jpg'
-    ]
-     
-  },
-  onLoad (options) {
-    let pages = getCurrentPages().length; 
-    let navData = {
-      title:'精选',
-      pages,
-      bgcolor: "#3D74F3",
-      color:'#fff'
+    onLoad(options) {
+        let pages = getCurrentPages().length;
+        let navData = {
+            title: '精选',
+            pages,
+            bgcolor: "#3D74F3",
+            color: '#fff'
+        }
+        let height = getApp().globalData.height
+
+        // 获取网络状态
+        getType()
+            .then((res) => {
+                let type = false;
+                if (res == 'none') {
+                    navData.bgcolor = '#fff'
+                    navData.color = '#333'
+                } else {
+                    type = true
+                }
+                this.setData({
+                    navData,
+                    height,
+                    type
+                })
+            })
+    },
+    currentChange(e) {
+        let index = e.detail.current;
+        this.setData({
+            index
+        })
+    },
+
+    switch(e) {
+        let title = e.currentTarget.dataset.val
+        console.log(title)
+        this.setData({
+            title
+        })
+    },
+
+    // 固定元素
+    onPageScroll(e) {
+        let query = tt.createSelectorQuery();
+        query.select(".title").boundingClientRect((res) => {
+            let {height, oldTop, isFixed} = this.data;
+            let {top} = res;
+            if (top <= height * 2 + 24) {
+                isFixed = true;
+                this.setData({
+                    oldTop: e.scrollTop
+                })
+            } else if (e.scrollTop < oldTop) {
+                isFixed = false
+            }
+            console.log(isFixed);
+            this.setData({
+                isFixed
+            })
+        })
+        query.exec();
     }
-    let height = getApp().globalData.height
-
-    // 获取网络状态
-    getType()
-    .then((res) => {
-      let type = false;
-      if(res == 'none'){
-        navData.bgcolor = '#fff'
-        navData.color = '#333'
-      } else {
-        type = true
-      }
-      this.setData({
-        navData,
-        height,
-        type
-      })
-    })
-  },
-  currentChange(e){
-   let index = e.detail.current;
-   this.setData({
-    index
-   })
-  },
-
-  switch(e){
-  let title = e.currentTarget.dataset.val
-  console.log(title)
-   this.setData({
-    title
-   })
-  }
 
 })
